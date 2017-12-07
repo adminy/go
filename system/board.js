1275e7f16e27819fd91b4728b66a70188ad2e0d3
bivolm2 <marin.bivol2@mail.dcu.ie>
2017-12-07 12:01:11 +0000

    More Broken Logic Fixed

--- system/board.js
+++ system/board.js
@@ -16,7 +16,12 @@ class Board {
 		}
 		//draw empty board
 		this.drawBoard();
-		this.waitForUserEvents();
+		
+		this.canvas.addEventListener("mousedown", function(event) {
+			console.log("CX:"+event.clientX+" CY:"+event.clientY + " BR:"+JSON.stringify(board.rect))
+			board.checkvalidclick({x: event.clientX - board.rect.left,
+								   y: event.clientY - board.rect.top});
+		});
 
     }
 
@@ -27,8 +32,8 @@ class Board {
 		this.rect = this.canvas.getBoundingClientRect();
 		this.ctx = this.canvas.getContext('2d');
 		this.distance_between = this.canvas.width/this.board_size; //distance between spaces		
+		this.radius = this.distance_between/2;
 		
-
 		this.ctx.beginPath();
 		for(var i = 0; i < this.board_size; i++) {
 			this.ctx.moveTo(this.board_pos, this.board_pos + this.distance_between * i);
@@ -38,23 +43,18 @@ class Board {
 		}
 		this.ctx.closePath();
 		this.ctx.stroke();
-	
-	
-		//place positions
-		this.board_positions = [];
-		this.radius = this.distance_between/2;
-		
-		for(var y = this.board_pos; y <= this.distance_between * this.board_size; y += this.distance_between) {
-			//this.board_positions.push([]);
-			for(var x = this.board_pos; x <= this.distance_between*this.board_size; x += this.distance_between) {
+
+		//now draw the circles
+		for(var y = 0; y < this.board.length; y++) {
+			for(var x = 0; x < this.board[y].length; x++) {
 				this.ctx.beginPath();
-				this.ctx.arc(x, y, this.radius, 0, 2*Math.PI);
+				this.ctx.arc(this.board_pos + x * this.distance_between, this.board_pos + y * this.distance_between, this.radius, 0, 2*Math.PI);
 				this.ctx.closePath();
 				this.ctx.stroke();
-				this.board_positions.push([x,y, false]); //x, y, set on board or not?
 			}
-			i++;
 		}
+
+
 	}
 	/* This method removes empty chains from the board
 	*	-goes through every chain and checks if it has no stones
@@ -69,62 +69,40 @@ class Board {
 		newChains = [];
 	}
 
-	waitForUserEvents() {
-		this.canvas.addEventListener("mousedown", function(event) {
-				var mouse = {x: event.clientX - board.rect.left, y: event.clientY - board.rect.top};
-				board.checkvalidclick(mouse);
-		}, false);
-	}
 	/* move valid called by event listener, so is it the player's turn?	//check `me.canMove` {Bool}
 	*	-when its the user's turn and it is a valid move to make, like that position is empty
 	*	-then make a Turn using player class
 	*/
 	checkvalidclick(mouse) {
-		for(var i = 0; i < this.board_positions.length; i++) {
-			//ALTERNATIVE CODE
-			//if clicked inside a circle
-			//if(Math.sqrt((mouse.x - this.board_positions[i][0]) * (mouse.x - this.board_positions[i][0]) +
-			//	 (mouse.y - this.board_positions[i][1]) * (mouse.y - this.board_positions[i][1])) < 
-			//			this.radius && this.board_positions[i][2] !== true && me.canMove) {
-			
-			if(Math.pow(mouse.x - this.board_positions[i][0], 2) + Math.pow(mouse.y - this.board_positions[i][1], 2) < Math.pow(this.radius,2) && this.board_positions[i][2] !== true && me.canMove) {
-
-					me.makeTurn(parseInt(i/this.board_size), i%this.board_size); //move y, x
-					this.board_positions[i][2] = true;
-					//console.log("Y: "+parseInt(i/this.board_size)+", X: "+ (i%this.board_size) );
+		for(var y = 0; y < this.board.length; y++) {
+			for(var x = 0; x < this.board[y].length; x++) {
+				var my = this.board_pos + y * this.distance_between, 
+					mx = this.board_pos + x * this.distance_between;
+				//if clicked inside a circle
+				//if(Math.sqrt((mouse.x-mx)**2+(mouse.y-my)**2 < this.radius 
+				//	&& this.board[y][x] == "Free" && me.canMove))
+				if(mouse.x > mx - this.radius && mouse.x < mx + this.radius &&
+					mouse.y > my - this.radius && mouse.y < my + this.radius &&
+					 this.board[y][x] == "Free" && me.canMove)
+					me.makeTurn(y, x);
 			}
 		}
 	}
 
 	drawMove(colour, y, x) {
-		var i = y * this.board_size + x;
-		//console.log(i);
+		var my = this.board_pos + y * this.distance_between, 
+			mx = this.board_pos + x * this.distance_between;
 		this.ctx.beginPath();
-		this.ctx.arc(this.board_positions[i][0], this.board_positions[i][1], this.radius, 0, 2*Math.PI);
+		this.ctx.arc(mx, my, this.radius, 0, 2*Math.PI);
 		this.ctx.fillStyle = colour;
 		this.ctx.fill();
 		this.ctx.closePath();
-		this.board_positions[i][2] = true;
 	}
 
 
 	deleteMove(y, x) {
-		var i = y * this.board_size + x;
-
-		this.ctx.beginPath();
-		this.ctx.arc(this.board_positions[i][0], this.board_positions[i][1], this.radius, 0, 2*Math.PI);
-		
-		this.ctx.moveTo(this.board_positions[i][0] - this.radius,this.board_positions[i][0], this.radius,this.board_positions[i][1]);
-		this.ctx.lineTo(this.board_positions[i][0] + this.radius, this.board_positions[i][1] + this.radius);
-
-		this.ctx.moveTo(this.board_positions[i][1] - this.radius,this.board_positions[i][1], this.radius,this.board_positions[i][0]);
-		this.ctx.lineTo(this.board_positions[i][1] + this.radius, this.board_positions[i][0] + this.radius);
-
-		this.ctx.fillStyle = "#e1995e";
-		this.ctx.fill();
-		this.ctx.closePath();
-		this.board_positions[i][2] = false;
-		this.board[y][x] = "Free"; //something here	
+		this.board[y][x] = "Free";
+		this.update();
 	}
 
 	update() {
--- system/player.js
+++ system/player.js
@@ -11,35 +11,29 @@ class Player {
     }
 
     placeTurn(data) {
-        var move = data.split(',');
-        var move_colour = move[1], move_y = parseInt(move[2]), move_x = parseInt(move[3]);
-        board.drawMove(move_colour, move_y, move_x );
+        var move        = data.split(',');
+        var move_colour = move[1],
+            move_y      = parseInt(move[2]),
+            move_x      = parseInt(move[3]);
         board.board[move_y][move_x] = new Stone(move_colour, move_y, move_x);
-
         this.refreshLiberties();
         this.prisoners(move_y, move_x);
-
+        
         var history_div = document.getElementById("move_history");
         history_div.innerHTML += move[1] + " at y: " + move_y + " x: " + move_x + "<br>";
         history_div.scrollTop = history_div.scrollHeight;
-        //document.getElementById("move_history").innerHTML += move[1] + " at y: " + (parseInt(move[2])+1) + " x: " + (parseInt(move[2])) + "<br>";
-        //document.getElementById("move_history").scrollTop = document.getElementById("move_history").scrollHeight;
         board.update();
     }
 
-
     refreshLiberties() {
-        for(var y = 0; y < board.board.length; y++) {
-            for(var x = 0; x < board.board[y].length; x++) {
+        for(var y = 0; y < board.board.length; y++)
+            for(var x = 0; x < board.board[y].length; x++)
                 if(board.board[y][x] != "Free")
                     board.board[y][x].updateLiberties();
-            }
-        }
     }
 
 
     prisoners(y, x){
-
         //check suicide
         if (board.board[y][x].mychain.totalLiberties() == 0){
         // remove this chain
@@ -53,8 +47,6 @@ class Player {
                 if(board.chains[i].totalLiberties() == 0)
                     board.chains[i].removeChain();
                     //update my score by chain length
-
-
         }
     }
 
